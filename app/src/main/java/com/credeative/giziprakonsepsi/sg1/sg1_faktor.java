5ceb96e1380516b7e741ff167084b08583638576
Arkhi <arkhims@credeative.com>
2020-10-29 23:27:57 +0700

    MARI tinggal 1 Layour + logic

---
+++ app/src/main/java/com/credeative/giziprakonsepsi/sg1/sg1_faktor.java
@@ -0,0 +1,16 @@
+package com.credeative.giziprakonsepsi.sg1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.credeative.giziprakonsepsi.R;
+
+public class sg1_faktor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sg1_faktor);
+    }
+}
