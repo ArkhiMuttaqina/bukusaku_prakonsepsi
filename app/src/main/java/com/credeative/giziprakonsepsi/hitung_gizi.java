64d39351ccfa7b6706e1903d548c2b5ef93f963e
Arkhi <arkhims@credeative.com>
2020-11-01 19:23:46 +0700

    All layout done

---
+++ app/src/main/java/com/credeative/giziprakonsepsi/hitung_gizi.java
@@ -0,0 +1,16 @@
+package com.credeative.giziprakonsepsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hitung_gizi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hitung_gizi);
+        getSupportActionBar().hide();
+
+    }
+}
